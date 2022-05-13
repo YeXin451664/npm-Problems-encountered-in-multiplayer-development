8c820a85625779089c240eb3808a8321d6c82f3a
野心 <2282777860@qq.com>
2022-05-13 09:24:42 +0800

    初始化提交

--- 生成随机数.js
+++ 生成随机数.js
@@ -3,6 +3,7 @@
     const lodash = require("lodash")
     console.log(lodash.random(1,10));
 
+    console.log("输出一下我们的多人开发遇到的问题!")
     // 在多人协作开发的时候,一定要注意的一点就是千万不要将npm下载安装的包的node_modules文件夹存入git版本库的仓库区中,更不可以将这个文件夹提交到远程中心仓库中.
     // 配置git的忽略文件提交
     
